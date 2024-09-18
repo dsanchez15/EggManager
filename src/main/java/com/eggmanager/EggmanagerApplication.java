ec5f2ba6edea18d28d3113249f145d97c47af8a8
Duvan Sanchez <duvan.sanchez@dreamcodesoft.com>
2024-09-17 22:33:35 -0500

    First commit.

---
+++ src/main/java/com/eggmanager/EggmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.eggmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggmanagerApplication.class, args);
+	}
+
+}
