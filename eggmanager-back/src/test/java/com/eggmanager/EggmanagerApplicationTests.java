3d68734180d4c11fb742ceb96e101ec4fa2ea095
Duvan Sanchez <duvan.sanchez@dreamcodesoft.com>
2025-09-04 00:07:51 -0500

    EM-00001: First commit. I created the project and added the connection to H2.

---
+++ eggmanager-back/src/test/java/com/eggmanager/EggmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eggmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EggmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
